199ae457e27ddc61cd659a083f4170123a13b0d4
Bhumika Chouhan <bhumi200526@gmail.com>
2026-01-25 15:24:04 +0530

    Add login sign

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,17 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
-
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 // MongoDB connect
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
+console.log(PORT);
+
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
